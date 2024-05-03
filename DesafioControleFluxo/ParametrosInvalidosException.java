5cc3c1d38b3b5db400c69daa69cea3fb06bd708b
Dino-back <142909179+Dino-back@users.noreply.github.com>
2024-05-03 01:22:28 -0300

    Delete DesafioControleFluxo directory

--- DesafioControleFluxo/ParametrosInvalidosException.java
+++
@@ -1,5 +0,0 @@
-public class ParametrosInvalidosException extends exception{
-    public ParametrosInvalidosException() {
-        super("O segundo parâmetro deve ser maior que o primeiro");
-    }
-}
